4a98558e3733ede7d8943ed1f17ecf3abf85dc07
Thomas Bauer <tb094@hdm-stuttgart.de>
2016-04-26 12:27:55 +0200

    commit pewpew

---
+++ Seeder.java
@@ -0,0 +1,89 @@
+public class Seeder {
+
+	
+	/* !!Was ist das und warum?
+	 * Der Seeder füllt die DB mit Dummy-Daten. Denn um zu Testen sind 
+	 * Testdaten sehr praktisch:
+	 * 
+	 * 1. Im Laufe der Entwicklung muss die Datenbank immer wieder neu 
+	 * aufgesetzt werden: zB weil neue Spalten hinzugefügt oder gelöscht 
+	 * werden müssen. Jedes Mal neue Daten einzugeben ist sehr anstrengend.
+	 * 
+	 * 2. Mit Testdaten lässt sich die Funktionalität der Datenbank/Applikation 
+	 * schnell testen.Wenn Tabellen und Daten nicht richtig zusammenpassen, 
+	 * fällt das mit einem Seeder schnell auf.
+	 */
+
+	/** !!Wie es funktioniert:
+	 *  Für jede MapperKlasse (bsp: SomeObjectMapper) braucht es einen Block,
+	 *  der in der Vorlage eingerahmt wird von den Kommentaren 'someTable start' 
+	 *  und 'someTable end'. Jeder Block besteht aus einer Methode (bsp: seedSomeTable()), 
+	 *  die die Datenbank füllt und Arrays aus den nötigen Werten (bsp: 'someValues').
+	 *  
+	 *  Die seedSomeTable() Methode ruft in der entsprechenden MapperKlasse
+	 *  (bsp: SomeObjectMapper) die insert() Methode auf (bsp: insertSObject()).
+	 *  
+	 *  Achtung: es gibt 2 Möglichkeiten: 
+	 *  M1: eure insert-Methode nimmt ein Object. So macht es der Thies. 
+	 *  (siehe AccountMapper.insert() @Bank2.0)
+	 *  M2: eure insert-Methode nimmt alle Parameter einzeln.
+	 */
+	
+	/* !!HowTo:
+	 * 
+	 * 0. In das Package mit den MapperKlassen diese SeederKlasse implementieren.
+	 * 
+	 * 1. ein neuen Block starten. zB **** profilTable start ****
+	 * 
+	 * 2. eine Methode schreiben. zB seedProfilTable()
+	 * 
+	 * 3. für jedes Attribut des BusinessObjects einen Array anlegen
+	 * 	  mit den benötigten Werten. zB 10xVornamen, 10xNachnamen und 10xAlter
+	 * 
+	 * 4. Eine Schleife in die seedProfilTable() schreiben, die die Mapper.insert()
+	 *    benutzt und dafür die Werte aus den Arrays aus 3. benutzt.
+	 *    
+	 * 5. Die neue Methode aus 2. in die seed() Methode schreiben.
+	 * 
+	 * 6. Von wo auch immer die seed() Methode aufrufen. Die ruft alle andern Methoden
+	 *    auf und befüllt eure Datenbank.
+	 *    
+	 * 7. Sich freuen wie n Iltis! 
+	 * 
+	 */
+	
+	public void seed(){
+		seedSomeTable();
+	}
+	
+	/************* someTable start *******************/
+	public void seedSomeTable(){
+		for(int i = 0; i < someValues.length; i++){
+			//M1
+			SomeObject so = new SomeObject();
+			so.setAlter(someValues[i]);
+			so.setFname(someOtherValues[i]);
+			SomeObjectMapper.someObjectMapper().insertSObject(so);
+			
+			//M2 (Anm.: entweder M1 oder M2)
+			ProfilMapper.profilMapper().insertProfil(
+							someValues[i], someOtherValues[i]);
+		}
+	}
+	
+	private int[]someValues = {
+		5, 6, 7, 12, 54, 2, 98, 1, 54, 90
+	};
+	
+	private String[]someOtherValues = {
+		"Thomas", "Hanna", "Dieter", "Gerd", "Mike",
+		"Kerstin", "Anna", "Peter", "Martin", "Tim"	
+	};
+	
+	/************* someTable end *******************/
+
+	/************* someOtherTable start *******************/
+	//genau wie bei 'someTable'
+
+
+}
